423078d4da9712d0c1b327cdd60b792f80890a96
Risu <79110363+risu729@users.noreply.github.com>
2024-01-25 21:19:07 +0900

    chore(knip.config.ts): remove unused ignoreDependencies (#41)

--- knip.config.ts
+++ knip.config.ts
@@ -1,12 +1,6 @@
 import type { KnipConfig } from "knip";
 
 const config: KnipConfig = {
-	ignoreDependencies: [
-		"bun",
-		// @commitlint/cli cannot be detected because its binary is named "commitlint"
-		// ref: https://knip.dev/guides/handling-issues/#example
-		"@commitlint/cli",
-	],
 	ignoreBinaries: ["screen"],
 };
 
