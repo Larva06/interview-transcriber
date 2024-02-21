80ca65f293da38fb1ce1cd29d14c727641c79294
Risu <79110363+risu729@users.noreply.github.com>
2024-02-22 00:30:52 +0900

    fix(src/env.d.ts): declare envs as readonly (#106)

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,39 +3,33 @@ declare module "bun" {
 		/**
 		 * Token of the Discord bot.
 		 */
-		// biome-ignore lint/style/useNamingConvention: should be SCREAMING_SNAKE_CASE
-		DISCORD_BOT_TOKEN: string;
+		readonly DISCORD_BOT_TOKEN: string;
 
 		/**
 		 * ID of the Discord guild where the bot is used.
 		 */
-		// biome-ignore lint/style/useNamingConvention:
-		DISCORD_GUILD_ID: string;
+		readonly DISCORD_GUILD_ID: string;
 
 		/**
 		 * Email of the Google Cloud service account.
 		 * (`client_email` in the JSON file)
 		 */
-		// biome-ignore lint/style/useNamingConvention:
-		GOOGLE_SERVICE_ACCOUNT_EMAIL: string;
+		readonly GOOGLE_SERVICE_ACCOUNT_EMAIL: string;
 
 		/**
 		 * Private key of the Google Cloud service account.
 		 * (`private_key` in the JSON file)
 		 */
-		// biome-ignore lint/style/useNamingConvention:
-		GOOGLE_SERVICE_ACCOUNT_KEY: string;
+		readonly GOOGLE_SERVICE_ACCOUNT_KEY: string;
 
 		/**
 		 * API key of the OpenAI API.
 		 */
-		// biome-ignore lint/style/useNamingConvention:
-		OPENAI_API_KEY: string;
+		readonly OPENAI_API_KEY: string;
 
 		/**
 		 * API key of the Gemini API.
 		 */
-		// biome-ignore lint/style/useNamingConvention:
-		GEMINI_API_KEY: string;
+		readonly GEMINI_API_KEY: string;
 	}
 }
