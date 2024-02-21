17b9e889450a0b640bd2ef17972da76f984fd987
risu729 <79110363+risu729@users.noreply.github.com>
2024-02-21 20:44:21 +0900

    chore: do not omit run in bun run commands

--- knip.config.ts
+++ knip.config.ts
@@ -1,6 +1,11 @@
 import type { KnipConfig } from "knip";
 
-const config: KnipConfig = {};
+const config: KnipConfig = {
+	ignoreDependencies: [
+		// bun run is not handled correctly by script parser
+		"@commitlint/cli",
+	],
+};
 
 // biome-ignore lint/style/noDefaultExport:
 export default config;
